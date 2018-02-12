dac9964d0fc508278b66cbf8caf74e5f88b9d4d8
SoojiNoh <thensj6873@gmail.com>
2018-02-13 06:28:01 +0900

    implemented image drag and move on cameraView

--- app/src/main/java/com/example/soojinoh/pickarchu/MainActivity.java
+++ app/src/main/java/com/example/soojinoh/pickarchu/MainActivity.java
@@ -1,13 +1,77 @@
 package com.example.soojinoh.pickarchu;
 
-import android.support.v7.app.AppCompatActivity;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener, View.OnDragListener {
+
+    private Camera mCamera = null;
+    private CameraView mCameraView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.imageView).setOnTouchListener(this);
+        findViewById(R.id.imageView).getRootView().setOnDragListener(this);
+
+        try{
+            mCamera = Camera.open();//you can use open(int) to use different cameras
+        } catch (Exception e){
+            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
+        }
+
+        if(mCamera != null) {
+            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
+            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
+            camera_view.addView(mCameraView);//add the SurfaceView to the layout
+
+        }
+
+        //btn to close the application
+        ImageButton imgClose = (ImageButton)findViewById(R.id.imgClose);
+        imgClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
+    }
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+            view.startDrag(null, shadowBuilder, view, 0);
+            view.setVisibility(View.GONE);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean onDrag(View v, DragEvent event) {
+
+        switch (event.getAction()) {
+            case DragEvent.ACTION_DROP:
+
+                float X = event.getX();
+                float Y = event.getY();
+
+                Log.d("Logcat", "X " + (int) X + "Y " + (int) Y);
+                View view = (View) event.getLocalState();
+                view.setX(X-(view.getWidth()/2));
+                view.setY(Y-(view.getHeight()/2));
+                view.setVisibility(View.VISIBLE);
+
+            default:
+                break;
+        }
+        return true;
     }
 }
