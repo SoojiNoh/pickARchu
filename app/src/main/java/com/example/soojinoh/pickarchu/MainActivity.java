1504ca8dcc226a368f05ed733b85ccf743c9a0cb
Tsujiku <tsujiku3011@outlook.kr>
2018-02-13 09:11:19 +0900

    modified photo path

--- app/src/main/java/com/example/soojinoh/pickarchu/MainActivity.java
+++ app/src/main/java/com/example/soojinoh/pickarchu/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.soojinoh.pickarchu;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -7,7 +8,9 @@ import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,14 +19,23 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    private final int GALLERY_CODE = 1111;
+    private final int CAMERA_CODE = 1111;
+    private final int GALLERY_CODE=1112;
+    private static final int PICK_FROM_CAMERA = 1;
+    private static final int PICK_FROM_GALLERY = 2;
+
     private Uri photoUri;
     private String currentPhotoPath;//실제 사진 파일 경로
     String mImageCaptureName;//이미지 이름
     ImageView ivImage;
+    ImageView imgview;
+    String address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +43,83 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //ivImage = (ImageView) findViewById(R.id.imageView) ;
         ivImage = (ImageView) findViewById(R.id.imageView) ;
-
         Button buttonGallery = (Button) findViewById(R.id.btn_select_gallery);
+
 
         buttonGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                selectGallery();
 
+                selectGallery();
+                /*Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                try {
+                    intent.putExtra("return-data", true);
+                    startActivityForResult(Intent.createChooser(intent, "Complete action using"), PICK_FROM_GALLERY);
+                } catch (ActivityNotFoundException e){
+
+                }*/
             }
         });
+    }
+    /*
+        protected void onActivityResult(int requestCode,
+                                        int resultCode,
+                                        Intent data) {
+
+            if (requestCode == PICK_FROM_CAMERA) {
+                Bundle extras = data.getExtras();
+                if (extras != null) {
+                    Bitmap photo = extras.getParcelable("data");
+                    imgview.setImageBitmap(photo);
+                }
+            }
+            if (requestCode == PICK_FROM_GALLERY) {
+                Bundle extras2 = data.getExtras();
+                if (extras2 != null) {
+                    Bitmap photo = extras2.getParcelable("data");
+                    imgview.setImageBitmap(photo);
+                }
+            }
+        }
+    */
+    private void selectPhoto() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException ex) {
+
+                }
+                if (photoFile != null) {
+                    photoUri = FileProvider.getUriForFile(this, getPackageName(), photoFile);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                    startActivityForResult(intent, CAMERA_CODE);
+                }
+            }
+
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        File dir = new File(Environment.getExternalStorageDirectory() + "/path/");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        mImageCaptureName = timeStamp + ".png";
+
+        File storageDir = new File(Environment.getExternalStorageDirectory().getAbsoluteFile() + "/path/"
+
+                + mImageCaptureName);
+        currentPhotoPath = storageDir.getAbsolutePath();
+
+        return storageDir;
+
     }
 
     //step1. 인텐트로 갤러리 호출
@@ -49,6 +128,7 @@ public class MainActivity extends AppCompatActivity {
         intent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         intent.setType("image/*");
         startActivityForResult(intent, GALLERY_CODE);
+
     }
 
     //step2. 갤러리에서 가져온 사진 사용
@@ -61,8 +141,13 @@ public class MainActivity extends AppCompatActivity {
             switch (requestCode) {
 
                 case GALLERY_CODE:
-                    sendPicture(data.getData()); //갤러리에서 가져오기
+                    address = sendPicture(data.getData()); //갤러리에서 가져오고 이미지의 path 리턴
+                    Log.d("***sssss***", address);
                     break;
+                case CAMERA_CODE:
+                    //getPictureForPhoto(); //카메라에서 가져오기
+                    break;
+
                 default:
                     break;
             }
@@ -70,10 +155,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void sendPicture(Uri imgUri) {
+    private String sendPicture(Uri imgUri) {
 
         String imagePath = getRealPathFromURI(imgUri); // path 경로
         ExifInterface exif = null;
+        address = imagePath;
         try {
             exif = new ExifInterface(imagePath);
         } catch (IOException e) {
@@ -83,15 +169,38 @@ public class MainActivity extends AppCompatActivity {
         int exifDegree;
         //int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
         //int exifDegree = exifOrientationToDegrees(exifOrientation);
+//        if (exif != null) {
+//            exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+//            exifDegree = exifOrientationToDegrees(exifOrientation);
+//        } else {
+//            exifDegree = 0;
+//            Toast.makeText(MainActivity.this, "gngn", Toast.LENGTH_SHORT);
+//            Log.i("ddddd","fffffffffffff");
+//        }
+        Bitmap bitmap = BitmapFactory.decodeFile(imagePath);//경로를 통해 비트맵으로 전환
+        ivImage.setImageBitmap(bitmap);
+
+        return imagePath;
+        //ivImage.setImageBitmap(rotate(bitmap, exifDegree));//이미지 뷰에 비트맵 넣기
+    }
+
+    private void getPictureForPhoto() {
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath);
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(currentPhotoPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int exifOrientation;
+        int exifDegree;
+
         if (exif != null) {
             exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
             exifDegree = exifOrientationToDegrees(exifOrientation);
         } else {
             exifDegree = 0;
-            Toast.makeText(MainActivity.this, "gngn", Toast.LENGTH_SHORT);
-            Log.i("ddddd","fffffffffffff");
         }
-        Bitmap bitmap = BitmapFactory.decodeFile(imagePath);//경로를 통해 비트맵으로 전환
         ivImage.setImageBitmap(rotate(bitmap, exifDegree));//이미지 뷰에 비트맵 넣기
     }
 
@@ -128,4 +237,7 @@ public class MainActivity extends AppCompatActivity {
 
         return cursor.getString(column_index);
     }
+
+
+
 }
